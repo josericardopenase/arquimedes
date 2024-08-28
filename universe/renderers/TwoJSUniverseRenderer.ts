a45f51ee18f472be5a74cdb14a5d3b78569bc3c0
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-08-28 18:57:10 +0100

    zoom in and out implemented

--- universe/renderers/TwoJSUniverseRenderer.ts
+++ universe/renderers/TwoJSUniverseRenderer.ts
@@ -1,16 +1,20 @@
 import UniverseRenderer from "../universeRenderer";
 import Two from "two.js";
+import { Group } from "two.js/src/group";
 import { Particle, Rigidbody} from "../../physics";
 
 export default class TwoJSUniverseRenderer implements UniverseRenderer {
     private two: Two
+    private worldContainer: Group;
 
     constructor() {
         var params = {
             fullscreen: true
         };
         this.two = new Two(params).appendTo(document.body);
+        this.worldContainer = this.two.scene;
         this.drawGrid()
+        this.addZoomSupport()
     }
 
     drawRigidbody(rb: Rigidbody): void {
@@ -20,16 +24,39 @@ export default class TwoJSUniverseRenderer implements UniverseRenderer {
     private drawGrid(){
     }
 
+    private addZoomSupport() {
+        document.addEventListener('wheel', (e) => {
+            e.preventDefault();
+
+            const scaleFactor = e.deltaY < 0 ? 1.1 : 0.9;
+            const mousePosition = { x: e.clientX, y: e.clientY };
+
+            const localPos = {
+                x: (mousePosition.x - this.worldContainer.translation.x) / Number(this.worldContainer.scale),
+                y: (mousePosition.y - this.worldContainer.translation.y) / Number(this.worldContainer.scale),
+            };
+
+            this.worldContainer.scale = Number(this.worldContainer.scale) * scaleFactor;
+
+            this.worldContainer.translation.x = mousePosition.x - (localPos.x * this.worldContainer.scale);
+            this.worldContainer.translation.y = mousePosition.y - (localPos.y * this.worldContainer.scale);
+
+            this.two.update();
+        });
+    }
+
     clear(): void {
         this.two.clear()
     }
+
     render(): void {
-        this.two.render()
+        this.two.update()
     }
 
     drawParticle(p: Particle) {
         const square = this.two.makeRectangle(p.position.x, p.position.y, p.apparience.width, p.apparience.height)
         square.fill = p.apparience.color
-        this.two.render()
+        this.worldContainer.add(square)
+        this.two.update()
     }
 }
